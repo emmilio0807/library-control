b57f50fb4178ca6f3c03dd8118971884d4c9e6e1
emmilio0807 <forplaylol@outlook.es>
2025-08-16 18:42:41 -0400

    integracion de modulo buscar_libros

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ class Libro:
                 nombre,apellido = autor.split()
                 return apellido[0]+nombre[0]+titulo[0]+str(next(numeros))
             except:
-                raise ValueError("Debe ingrese autor con el siguiente formato: 'nombre' 'apellido'")
+                raise ValueError("Debe ingresar autor con el siguiente formato: 'nombre' 'apellido'")
         
         self.id = asignador_id(titulo,autor) 
         self.titulo = titulo
@@ -37,6 +37,22 @@ class Biblioteca:
         libro = Libro(titulo,autor,genero,fecha_publicacion)
         self.libros.append(libro)
     
+    def buscar_libro(self,parametro):
+        resultado = []
+        for libro in self.libros:
+            if (parametro == libro.id or
+                parametro == libro.autor or
+                parametro == libro.genero or
+                parametro == libro.titulo or
+                parametro == libro.fecha_publicacion):
+                resultado.append(libro)
+        if not resultado:
+            return "No se encontraron resultados"
+        else: 
+            for n, libro in enumerate(resultado,1):
+                print(f"{n}. [{libro.id}] {libro.titulo} - {libro.autor} - {libro.genero}, ({libro.fecha_publicacion}) Prestado: {libro.prestado}")
+        return ""
+    
     def eliminar_libro(self,id):
         for libro in self.libros:
             if libro.id == id:
@@ -51,4 +67,8 @@ class Biblioteca:
 
 biblioteca1 = Biblioteca()
 
+biblioteca1.agregar_libro("Administracion","Juan","Terror",2005)
+biblioteca1.agregar_libro("Administracion","Miguel Angel","Fantasia",2010)
+
+print(biblioteca1.buscar_libro("Administracion"))
 
