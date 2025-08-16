4f7a687371f0a18398df354a55d3690a64225155
emmilio0807 <forplaylol@outlook.es>
2025-08-16 17:56:23 -0400

    update-id

--- src/main.py
+++ src/main.py
@@ -5,9 +5,17 @@ import itertools
 class Libro:
     
     def __init__(self,titulo:str="",autor:str="",genero:str="",fecha_publicacion:int=None):
-        nombre,apellido = autor.split()
-        numeros = itertools.count(0)
-        self.id = apellido[0]+nombre[0]+titulo[0]+str(next(numeros))
+        def asignador_id(titulo:str,autor:str):
+            numeros = itertools.count(0)
+            if len(autor.split()) != 2:
+                return autor[0]+titulo[0]+str(next(numeros))
+            try:
+                nombre,apellido = autor.split()
+                return apellido[0]+nombre[0]+titulo[0]+str(next(numeros))
+            except:
+                raise ValueError("Debe ingrese autor con el siguiente formato: 'nombre' 'apellido'")
+        
+        self.id = asignador_id(titulo,autor) 
         self.titulo = titulo
         self.autor = autor
         self.genero = genero
@@ -34,18 +42,13 @@ class Biblioteca:
             if libro.id == id:
                 self.libros.remove(libro)
     
-    def mostrar_libros(self):
+    def listar_libros(self):
         if not self.libros:
-            return "La biblioteca esta vacía"
+            return "\nLa biblioteca esta vacía"
         for n, libro in enumerate(self.libros,1):
             print(f"{n}. [{libro.id}] {libro.titulo} - {libro.autor} - {libro.genero}, ({libro.fecha_publicacion}) Prestado: {libro.prestado}")
         return ""
 
 biblioteca1 = Biblioteca()
-
-biblioteca1.agregar_libro("Principios de administracion","Adalberto Chiavenato","Educacion",2010)
-
-print(biblioteca1.mostrar_libros())
-
 
 
